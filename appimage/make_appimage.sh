5d3701a91c9883be08e1d510987497bfad8182c0
smokejohn <johannesrauch91@gmail.com>
2020-12-23 04:26:23 +0100

    #1 fix AppImage not working due to missing Qt plugin libraries

--- appimage/make_appimage.sh
+++ appimage/make_appimage.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# needs linuxdeploy and linuxdeploy-plugin-qt
+# needs appimagetool
+
 # Make sure to delete the appdir if building fails, subsequent execution of linuxdeploy will reuse existing configs and data otherwise
 
-linuxdeploy --appdir ./appdir -e ../build/bin/qaddapp -d ./qaddapp.desktop -i ./qaddapp_logo_256.png --custom-apprun=./AppRun
+linuxdeploy --appdir ./appdir -e ../build/bin/qaddapp -d ./qaddapp.desktop -i ./qaddapp_logo_256.png --custom-apprun=./AppRun -p qt
 
 ARCH=x86_64 appimagetool ./appdir/
